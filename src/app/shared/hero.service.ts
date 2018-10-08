aa75b4e432f0ca35336c04b628a348d3b954529a
Dennis <kigen.work@gmail.com>
2018-10-08 10:19:55 +0300

    Added HeroService

---
+++ src/app/shared/hero.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Hero } from './hero';
+
+const BASE_URL = 'http://localhost:3000/heroes';
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  constructor(private http: HttpClient) {}
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(BASE_URL);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    const url = `${BASE_URL}/${id}`;
+
+    return this.http.get<Hero>(url);
+  }
+
+  createHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(BASE_URL, hero, httpOptions);
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    const url = `${BASE_URL}/${hero.id}`;
+
+    return this.http.put(url, hero, httpOptions);
+  }
+
+  deleteHero(hero: Hero | Number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${BASE_URL}/${id}`;
+
+    return this.http.delete<Hero>(url, httpOptions);
+  }
+}
