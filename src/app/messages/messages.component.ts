6e732a5b7209e7b44b55f30a7bb01dd7817c33ad
Dennis Kigen <kigen.work@gmail.com>
2020-02-10 09:18:41 +0300

    Bump cli and core to Angular v9

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -8,7 +8,7 @@ import { MessageService } from '../shared';
 })
 export class MessagesComponent implements OnInit {
 
-  constructor(private messageService: MessageService) { }
+  constructor(public messageService: MessageService) { }
 
   ngOnInit() {
   }
